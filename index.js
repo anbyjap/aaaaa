4e50c80ebf1524a13cb74e8b6beb14b93bdf06b1
unknown <[haruru898@gmail.com]>
2020-07-17 06:33:06 +0900

    Initialize repository

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const doc = document.querySelector('doc'); 
-
-console.log(doc.textContent);
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('hamburger').addEventListener('click',function(){
+        var nav = document.getElementById('nav-bar');
+        if(nav.style.display == "block"){
+            nav.style.display = "none";
+        }
+        else {
+            nav.style.display = "block";
+        }
+    });
+});
